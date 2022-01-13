24bbbd96f3308b5e50c69ae65825e334d2143b6d
edunogueira <eduardo.n.oliveira@outlook.com>
2022-01-12 21:13:25 -0300

    v0.4 - clock and datatables

--- DOGenieAssistant.user.js
+++ DOGenieAssistant.user.js
@@ -1,14 +1,17 @@
 // ==UserScript==
 // @name         DO Genie Assistant
-// @version      0.3
+// @version      0.4
 // @namespace    https://github.com/edunogueira/DOGenieAssistant/
 // @description  Dugout-online genie assistant
 // @author       Eduardo Nogueira de Oliveira
 // @icon         https://www.google.com/s2/favicons?domain=dugout-online.com
 // @require	     http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
+// @require      https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js
 // @include      http*dugout-online.com/players/details*
 // @include      http*dugout-online.com/players/none*
 // @include      http*dugout-online.com/tactics/none*
+// @include      http*dugout-online.com/tactics_youth/none*
+// @include      http*dugout-online.com/*
 // ==/UserScript==
 
 var page = document.URL;
@@ -16,9 +19,29 @@ if (page.match('players/details/')) {
     playerDetails();
 } else if (page.match('players/none/')) {
     squadDetails();
-} else if (page.match('tactics/none/')) {
+} else if (page.match('tactics/none/') || page.match('tactics_youth/none/')) {
     tacticsDetails();
 }
+//second clock
+$('#footer').append('<div class="time_display" id="servertime2" style="top: 12px;border:1px solid #fff"></div>');
+
+function serverTime() {
+    const d = new Date();
+    let h = addZero(d.getHours());
+    let m = addZero(d.getMinutes());
+    let s = addZero(d.getSeconds());
+    let time = h + ":" + m + ":" + s;
+    $('#servertime2').text(time);
+}
+
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i
+    }
+    return i;
+}
+
+setInterval(serverTime, 1000);
 
 function playerDetails() {
     var data = Array();
@@ -49,14 +72,13 @@ function playerDetails() {
     } else if (position[8] == "1") { //FC
         ops = (data[3] + data[8] + data[17] + data[11] + data[13]);
     }
-    $('.player_name').append(' @ OPS '+ops+' | ' + exp + ' XP');
+    $('.player_name').append(' @ OPS ' + ops + ' | ' + exp + ' XP');
     $(document).prop('title', $('.player_name').text());
 };
 
-function getExp (string1)
-{
+function getExp(string1) {
     var retval = string1.substring(0, string1.indexOf(" XP\""));
-    retval = retval.substring(retval.lastIndexOf("\"")+"\"".length);
+    retval = retval.substring(retval.lastIndexOf("\"") + "\"".length);
     retval = parseInt(retval);
     return retval;
 }
@@ -161,18 +183,43 @@ function squadDetails() {
             } else if (position == "FC") {
                 ops = (data[3] + data[8] + data[17] + data[11] + data[13]);
             }
-            $(this).last().append('<td align="center"><span class="tableText">'+ops+'</span></td>');
+            $(this).last().append('<td align="center"><span class="tableText">' + ops + '</span></td>');
         }
     });
+
+    var tables = document.querySelectorAll("[class=forumline]");
+    tables[0].classList.add("gks");
+    tables[1].classList.add("dcs");
+    tables[2].classList.add("mcs");
+    tables[3].classList.add("pls");
+    doTable('.forumline.gks');
+    doTable('.forumline.dcs');
+    doTable('.forumline.mcs');
+    doTable('.forumline.pls');
 }
 
+function doTable(selector) {
+    $(selector + ' tr:first td').wrapInner('<div />').find('div').unwrap().wrap('<th/>');
+    var header = $(selector + " .table_top_row:first").clone();
+    $(selector + " .table_top_row:first").remove();
+    $(selector + " tbody:first").before('<thead></thead>');
+    $(selector + " thead:first").append(header);
+    $(selector).dataTable({
+        "searching": false,
+        "bPaginate": false,
+        "bLengthChange": false,
+        "bFilter": true,
+        "bInfo": false,
+        "bAutoWidth": false
+    });
+}
 function tacticsDetails() {
     $(".player").each(function() {
         var data = Array();
         var i = 0;
         var playerId = $(this).attr('rel').split('|')[0]
 
-        $(this).find("#"+playerId+" table tr").each(function() {
+        $(this).find("#" + playerId + " table tr").each(function() {
             $(this).children('td').each(function() {
                 if ($.isNumeric($(this).text())) {
                     data.push(parseInt($(this).text()));
@@ -183,15 +230,18 @@ function tacticsDetails() {
                     i++;
                 } else if (i == 22) {
                     $(this).append("0");
-                    $(this).css({"font-size": "12px", "font-weight": "bold"});
+                    $(this).css({
+                        "font-size": "12px",
+                        "font-weight": "bold"
+                    });
                     $(this).addClass("ops");
                     i++;
                 }
             });
         });
         if (data.length > 0) {
-            var array = $(this).find(" [class*=matches_row] > td").map(function(){
-               return $.trim($(this).text());
+            var array = $(this).find(" [class*=matches_row] > td").map(function() {
+                return $.trim($(this).text());
             }).get();
             var position = array[2];
             var ops = 0;
@@ -210,7 +260,7 @@ function tacticsDetails() {
             } else if (position == "FC") {
                 ops = (data[3] + data[8] + data[17] + data[11] + data[13]);
             }
-            $(this).find("#"+playerId+" .ops").text(ops);
+            $(this).find("#" + playerId + " .ops").text(ops);
         }
     });
 }
